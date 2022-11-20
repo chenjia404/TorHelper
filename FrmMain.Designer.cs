409b26358cc58729aca4610c96451ccf028006dd
chenjia404 <chenjiablog@gmail.com>
2022-11-20 16:15:33 +0800

    简易的tor gui

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace TorHelper
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.lbl_network_type = new System.Windows.Forms.Label();
+            this.cmb_network_type = new System.Windows.Forms.ComboBox();
+            this.lbl_port = new System.Windows.Forms.Label();
+            this.txt_port = new System.Windows.Forms.TextBox();
+            this.txt_log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(55, 283);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "启动";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(185, 283);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 0;
+            this.btn_stop.Text = "停止";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // lbl_network_type
+            // 
+            this.lbl_network_type.AutoSize = true;
+            this.lbl_network_type.Location = new System.Drawing.Point(23, 24);
+            this.lbl_network_type.Name = "lbl_network_type";
+            this.lbl_network_type.Size = new System.Drawing.Size(58, 13);
+            this.lbl_network_type.TabIndex = 1;
+            this.lbl_network_type.Text = "连接方式:";
+            // 
+            // cmb_network_type
+            // 
+            this.cmb_network_type.FormattingEnabled = true;
+            this.cmb_network_type.Items.AddRange(new object[] {
+            "直连",
+            "snowflake"});
+            this.cmb_network_type.Location = new System.Drawing.Point(84, 21);
+            this.cmb_network_type.Name = "cmb_network_type";
+            this.cmb_network_type.Size = new System.Drawing.Size(121, 21);
+            this.cmb_network_type.TabIndex = 2;
+            // 
+            // lbl_port
+            // 
+            this.lbl_port.AutoSize = true;
+            this.lbl_port.Location = new System.Drawing.Point(22, 71);
+            this.lbl_port.Name = "lbl_port";
+            this.lbl_port.Size = new System.Drawing.Size(58, 13);
+            this.lbl_port.TabIndex = 3;
+            this.lbl_port.Text = "代理端口:";
+            // 
+            // txt_port
+            // 
+            this.txt_port.Location = new System.Drawing.Point(88, 68);
+            this.txt_port.Name = "txt_port";
+            this.txt_port.Size = new System.Drawing.Size(100, 20);
+            this.txt_port.TabIndex = 4;
+            this.txt_port.Text = "9050";
+            // 
+            // txt_log
+            // 
+            this.txt_log.Location = new System.Drawing.Point(25, 135);
+            this.txt_log.Multiline = true;
+            this.txt_log.Name = "txt_log";
+            this.txt_log.Size = new System.Drawing.Size(280, 130);
+            this.txt_log.TabIndex = 5;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 344);
+            this.Controls.Add(this.txt_log);
+            this.Controls.Add(this.txt_port);
+            this.Controls.Add(this.lbl_port);
+            this.Controls.Add(this.cmb_network_type);
+            this.Controls.Add(this.lbl_network_type);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Name = "FrmMain";
+            this.Text = "tor助手";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Label lbl_network_type;
+        private System.Windows.Forms.ComboBox cmb_network_type;
+        private System.Windows.Forms.Label lbl_port;
+        private System.Windows.Forms.TextBox txt_port;
+        private System.Windows.Forms.TextBox txt_log;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorHelper
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        Process process_tor;
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            Process[] pa = Process.GetProcesses();//获取当前进程数组。
+            int proc_amount = 0;
+            foreach (Process PTest in pa)
+            {
+                //关闭之前的进程
+                if (PTest.ProcessName == "tor")
+                {
+                    PTest.Kill();
+                }
+            }
+            string torcc_str = "ControlPort 9051\n" +
+                "DataDirectory ./Data\n" +
+                "GeoIPFile Data/Tor/geoip/geoip\n" +
+                "GeoIPv6File Data/Tor/geoip/geoip6\n" +
+                "AvoidDiskWrites 1\n"
+                ;
+            if(cmb_network_type.SelectedItem == "snowflake")
+            {
+                torcc_str += "UseBridges 1\n" +
+                    "Bridge snowflake 192.0.2.3:80 2B280B23E1107BB62ABFC40DDCC8824814F80A72 fingerprint=2B280B23E1107BB62ABFC40DDCC8824814F80A72 url=https://snowflake-broker.torproject.net.global.prod.fastly.net/ front=cdn.sstatic.net ice=stun:stun.l.google.com:19302,stun:stun.altar.com.pl:3478,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.sonetel.net:3478,stun:stun.stunprotocol.org:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn\n" +
+"ClientTransportPlugin snowflake exec Tor/PluggableTransports/snowflake-client.exe\n";
+            }
+
+            torcc_str += "SocksPort " + txt_port.Text + "\n";
+
+            WriteFile("./Tor/torrc", torcc_str);
+
+            //要执行的程序名称
+            process_tor = new Process();
+            process_tor.StartInfo.FileName = "./tor/tor.exe";
+            process_tor.StartInfo.Arguments = " -f ./Tor/torrc";
+            process_tor.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+            process_tor.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+            process_tor.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+            process_tor.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+            process_tor.StartInfo.CreateNoWindow = true;//不显示程序窗口
+
+            process_tor.OutputDataReceived += OnDataReceived;
+
+            process_tor.Start();//启动程序
+            process_tor.BeginOutputReadLine();
+            process_tor.StandardInput.AutoFlush = true;
+        }
+
+        /// <summary>
+        /// 写入文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="txt"></param>
+        public static void WriteFile(string path, string txt)
+        {
+            try
+            {
+                FileStream file = new FileStream(path, FileMode.Create);
+                StreamWriter sw = new StreamWriter(file);
+                sw.WriteLine(txt);
+                sw.Close();
+                file.Close();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void OnDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            AddItemToTextBox(e.Data);
+        }
+
+
+        delegate void AddItemToTextBoxDelegate(string str);
+
+        /// <summary>  
+        /// 在ListBox中追加状态信息  
+        /// </summary>  
+        /// <param name="str">要追加的信息</param>
+        private void AddItemToTextBox(string str)
+        {
+            if (str == null)
+                return;
+            if(txt_log.InvokeRequired)
+            {
+                AddItemToTextBoxDelegate d = AddItemToTextBox;
+                txt_log.Invoke(d, str);
+            }
+            else
+            {
+                txt_log.AppendText(str);
+                txt_log.AppendText(Environment.NewLine);
+                txt_log.ScrollToCaret();
+            }
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            process_tor.Kill();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            cmb_network_type.SelectedIndex = 1;
+            
+        }
+    }
+}
